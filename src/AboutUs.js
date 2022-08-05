8cc06dc8c6a3e90595794b5853d3e769babb29a7
Naimish <naimishbbhuva@gmail.com>
2022-08-05 15:32:03 +0530

    react router page nav done in index.js

---
+++ src/AboutUs.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./aboutUs.css";
+
+function AboutUs() {
+    return <div className="aboutUs">AboutUs</div>;
+}
+
+export default AboutUs;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home from "./Home.js";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AboutUs from "./AboutUs.js";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/aboutus" element={<AboutUs />} />
+        </Routes>
+    </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
