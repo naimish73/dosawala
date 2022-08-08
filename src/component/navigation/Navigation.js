f43f2f4a8c648122ed786bd9cade47196e323372
Naimish <naimishbbhuva@gmail.com>
2022-08-08 16:22:48 +0530

    navigation bar finished responsively

--- src/component/navigation/Navigation.js
+++ src/component/navigation/Navigation.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navigation.css";
 import logo from "../../assets/logo.png";
 import { Link } from "react-router-dom";
-import sideBar from "../../assets/header-navigationIcon.png";
+import sideBar1 from "../../assets/header-navigationIcon-mobile.png";
+import sideBar2 from "../../assets/header-navigationCrossIcon.png";
 
 function Navigation() {
+    const [sideBar, setSideBar] = useState(false);
+
     return (
         <>
             <div className="navigation">
@@ -24,16 +27,35 @@ function Navigation() {
                     <Link to="#">Menu</Link>
                     <Link to="#">Contact</Link>
                 </div>
-                <div className="navigation-sideBar">
-                    <img src={sideBar} alt="sidebar of navigation" />
+                <div className="navigation-sidebar">
+                    <img
+                        src={sideBar ? sideBar2 : sideBar1}
+                        alt="sidebar logo"
+                        onClick={() => {
+                            !sideBar ? setSideBar(true) : setSideBar(false);
+                        }}
+                    />
                 </div>
             </div>
-            {/* <div className="test">
-                <div className="test1">
-                    <img className="img1" src={logo} />
-                    <img className="img2" src={sideBar} />
+            {sideBar ? (
+                <div className="navigation-items-mobile">
+                    <Link
+                        className="navigation-items-mobile-link"
+                        to="/aboutus"
+                    >
+                        About Us
+                    </Link>
+                    <Link className="navigation-items-mobile-link" to="#">
+                        Franchise
+                    </Link>
+                    <Link className="navigation-items-mobile-link" to="#">
+                        Menu
+                    </Link>
+                    <Link className="navigation-items-mobile-link" to="#">
+                        Contact
+                    </Link>
                 </div>
-            </div> */}
+            ) : null}
         </>
     );
 }
