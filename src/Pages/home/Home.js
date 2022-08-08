e54925610dd1269d4563b418201902d2651cfba2
Naimish <naimishbbhuva@gmail.com>
2022-08-08 17:27:26 +0530

    deleting extra images

--- src/Pages/home/Home.js
+++ src/Pages/home/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./home.css";
 import Navigation from "../../component/navigation/Navigation.js";
+import FirstPage from "../../component/firstPage/FirstPage";
 
 function Home() {
     return (
         <div>
             <Navigation />
+            <FirstPage />
         </div>
     );
 }
