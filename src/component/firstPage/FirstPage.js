7196cb8600135350f566016c28e43413b427b2d7
Naimish <naimishbbhuva@gmail.com>
2022-08-09 13:11:06 +0530

    firstPage done, 2 img remaining to add

--- src/component/firstPage/FirstPage.js
+++ src/component/firstPage/FirstPage.js
@@ -1,8 +1,30 @@
 import React from "react";
 import "./firstPage.css";
+import wave from "../../assets/home-firstPage-wave.png";
 
 function FirstPage() {
-    return <div className="firstPage"></div>;
+    return (
+        <div className="firstPage">
+            <div className="firstPage-text">
+                <div className="firstPage-text-large">
+                    Welcome To Authentic <br /> South Indian Taste
+                </div>
+                <div className="firstPage-text-small">
+                    <p>
+                        Introducing first time ever! Unlimited South Indian Meal
+                    </p>
+                </div>
+            </div>
+            <div className="firstPage-button">
+                <button>See Menu</button>
+            </div>
+            <img
+                src={wave}
+                alt="wave for styling"
+                className="firstPage-waveImage"
+            />
+        </div>
+    );
 }
 
 export default FirstPage;
