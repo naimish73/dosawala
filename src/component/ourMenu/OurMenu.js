c74f8f2cc9c20c7c6c132c39a8e08e50585f0241
Naimish <naimishbbhuva@gmail.com>
2022-08-16 16:37:20 +0530

    vision and growth done

--- src/component/ourMenu/OurMenu.js
+++ src/component/ourMenu/OurMenu.js
@@ -3,6 +3,7 @@ import "./ourMenu.css";
 import dosaImage from "../../assets/home-ourMenu-dosaImage.png";
 import foodImage from "../../assets/home-ourMenu-foodImage.png";
 import arrow from "../../assets/home-ourStory-arrow.svg";
+import imageDining from "../../assets/home-ourMenu-imageDining.png";
 
 function OurMenu() {
     return (
@@ -75,9 +76,16 @@ function OurMenu() {
                     <img src={foodImage} alt="full dish food" />
                 </div>
                 <div className="ourMenu-vision-content">
-                    <div>Vision and Growth</div>
-                    <div>A Winning Franchise Team</div>
-                    <p>
+                    <div className="ourMenu-vision-content-head1">
+                        Vision and Growth
+                    </div>
+                    <div className="ourMenu-vision-content-head2">
+                        <span className="ourMenu-vision-content-head2-a">
+                            A
+                        </span>{" "}
+                        Winning Franchise Team
+                    </div>
+                    <p className="ourMenu-vision-content-para1">
                         Dosawala strives to provide its customers, the
                         healthiest and wholesome meal at a very affordable
                         prices and to generate prots for the benet of the
@@ -86,22 +94,46 @@ function OurMenu() {
                         focussing and providing exclusive support and service on
                         committed environment
                     </p>
-                    <p>
+                    <p className="ourMenu-vision-content-para2">
                         To spread the product across the globe through our chain
                         of retail outlets. Our brand chain special taste makers
                         and ingredients will spread a standard avor of taste
                         all over the country
                     </p>
-                    <p className="ourMenu-vision-content">
+                    <p className="ourMenu-vision-content-para3">
                         Know more
                         <img
-                            className="ourMenu-vision-content"
+                            className="ourMenu-vision-content-arrow"
                             src={arrow}
                             alt="arrow"
                         />
                         <img src={arrow} alt="arrow" />
                         <img src={arrow} alt="arrow" />
                     </p>
+                </div>
+            </div>
+            <div className="ourMenu-contact">
+                <div className="ourMenu-contact-content">
+                    <div className="ourMenu-contact-content-head1">
+                        contact for
+                    </div>
+                    <div className="ourMenu-contact-content-head2">
+                        Franchise Inquiry
+                    </div>
+                    <p className="ourMenu-contact-content-para1">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua. Ut enim ad minim veniam, quis nostrud
+                        exercitation ullamco laboris nisi ut aliquip ex ea
+                        commodo consequat. Duis aute irure dolor in
+                        reprehenderit in voluptate velit esse cillum dolore eu
+                        fugiat null
+                    </p>
+                    <p className="ourMenu-contact-content-mail"></p>
+                    <p className="ourMenu-contact-content-contact"></p>
+                </div>
+                <div className="ourMenu-contact-imageDining">
+                    <img src={imageDining} alt="dining table" />
                 </div>
             </div>
         </div>
